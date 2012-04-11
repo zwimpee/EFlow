bb8d56c09b367066a0e2c4045fe02ef6096ff949
meridian <>
2012-04-11 17:12:24 +0000

    First commit

---
+++ EcalEFlowAnalysis/relaunchJobs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ -f logs/$1/createHistoryjobs.runerror ] ; then
+echo "Relaunching runerror jobs"
+cat logs/$1/createHistoryjobs.runerror
+cat logs/$1/createHistoryjobs.runerror | awk '{printf "bsub -q 1nd -o logs/'$1'/createHistoryOutjob_%s.log -J %s < jobs/'$1'/createHistoryOut_%s.sh\n",$7,$7,$7}' | /bin/sh
+mv -f logs/$1/createHistoryjobs.runerror  logs/$1/createHistoryjobs.runerror
+fi
+
+if [ -f logs/$1/createHistoryjobs.runerror ] ; then 
+echo "Relaunching copyerror jobs"
+cat logs/$1/createHistoryjobs.copyerror
+cat logs/$1/createHistoryjobs.copyerror | awk '{printf "bsub -q 1nd -o logs/'$1'/createHistoryOutjob_%s.log -J %s < jobs/'$1'/createHistoryOut_%s.sh\n",$7,$7,$7}' | /bin/sh
+mv -f logs/$1/createHistoryjobs.copyerror logs/$1/createHistoryjobs.copyerror.old
+fi
