9628f83240a7dd23c2285185b16ffa18a448ce16
micheli <>
2012-05-23 14:30:15 +0000

    small fix

--- EcalEFlowAnalysis/makeLaserRatioExecute.C
+++ EcalEFlowAnalysis/makeLaserRatioExecute.C
@@ -2,7 +2,7 @@
   gROOT->Reset();
   gROOT->ProcessLine(".L makeLaserRatio.C++");
   gROOT->ProcessLine("makeLaserRatio t");
-  t.setFiles("histories_RUN2011AB_400Mnew_","histories_RUN2011AB_400MnewDUMMY_");
-  t.setOutFile("laserRatioTest.root");
+  t.setFiles("/xrootdfs/cms/local/meridian/EFlow/histories/histories_AlCaPhiSym_Run2012A-v1_RAW_EcalLaserAPDPNRatios_data_20120512_189410_193761_cbt_EcalLaserAlphas_EE_sic1_btcp128_1200M_noBsCorr_2012dataBsCorr_","/xrootdfs/cms/local/meridian/EFlow/histories/histories_AlCaPhiSym_Run2012A-v1_RAW_v4_800M_noBsCorr_2012dataBsCorr_");
+  t.setOutFile("PhotonicsV1overNormal_");
   gROOT->ProcessLine("t.Loop()");
 }
