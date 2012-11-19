c0db9b1f594398e863a0548cf39ff2cb5c4682cc
meridian <>
2012-11-19 19:11:23 +0000

    adding EE

--- EcalEFlowAnalysis/combineAlphaFits.py
+++ EcalEFlowAnalysis/combineAlphaFits.py
@@ -37,7 +37,8 @@ def set_palette(name="color", ncontours=999):
     ROOT.TColor.CreateGradientColorTable(npoints, s, r, g, b, ncontours)
     ROOT.gStyle.SetNumberContours(ncontours)
 
-def getlist(input):
+def getlist(input,det):
+    print det
     lst = {}
     for line in input.split("\n"):
         if line.startswith("##"):
@@ -46,28 +47,47 @@ def getlist(input):
         try:
             if len(l) < 5:
                 continue
-            ieta, iphi, alpha, alpha_err, comment = int(l[0]), int(l[1]), float(l[2]), float(l[3]), l[4]
+            if (det=="EB"):
+                if (len(l)>=5):
+                    ieta, iphi, alpha, alpha_err, comment = int(l[0]), int(l[1]), float(l[2]), float(l[3]), l[4]
+            elif (det=="EE"):
+                if (len(l)>=6):
+                    ix, iy, sign, alpha, alpha_err, comment = int(l[0]), int(l[1]), int(l[2]), float(l[3]), float(l[4]), l[5]
         except Exception, e:
             print line
             print e
-        lst[ (ieta,iphi) ] = ( alpha, alpha_err, comment )
+        if (det=="EB" and len(l)>=5):
+            lst[ (ieta,iphi) ] = ( alpha, alpha_err, comment )
+        elif (det=="EE" and len(l)>=6):
+            lst[ (ix,iy,sign) ] = ( alpha, alpha_err, comment )
     return lst
 
-def fillDiffPlots(channels,lista,listb,scatter,diff,diffMap):
+def fillDiffPlots(channels,lista,listb,scatter,diff,diffMap,det):
     for channel in channels:
         diff.Fill(lista[channel][0]-listb[channel][0])
         scatter.Fill(lista[channel][0],listb[channel][0])
-        etaIndex=channel[0]+86
-        diffMap.SetBinContent(channel[1],etaIndex,(lista[channel][0]-listb[channel][0])+1.)
+        if (det=="EB"):
+            etaIndex=channel[0]+86
+            diffMap.SetBinContent(channel[1],etaIndex,(lista[channel][0]-listb[channel][0])+1.)
+        elif (det=="EE"):
+            ixIndex=channel[0]+channel[2]*100
+            diffMap.SetBinContent(ixIndex,channel[1],(lista[channel][0]-listb[channel][0])+1.)
 
-def fillAlphaPlots(list,values,err,map):
+def fillAlphaPlots(list,values,err,map,det):
     for channel in list.keys():
         #        if ( list[channel][3] != "ok" and list[channel][3] != "large_err_fit" ):
         #            continue
+        if (list[channel][0]<=0.7 or (det=="EB" and list[channel][0]==1.52) or (det=="EE" and list[channel][0]==1.16) or  list[channel][0]>=2.1):
+            continue
         values.Fill(list[channel][0])
         err.Fill(list[channel][1])
-        etaIndex=channel[0]+86
-        map.SetBinContent(channel[1],etaIndex,list[channel][0])
+        if (det=="EB"):
+            etaIndex=channel[0]+86
+            map.SetBinContent(channel[1],etaIndex,list[channel][0])
+            
+        elif (det=="EE"):
+            ixIndex=channel[0]+channel[2]*100
+            map.SetBinContent(ixIndex,channel[1],list[channel][0])
 
 def printDiffPlots(scatter,diff,diffMap,suffix):
     ROOT.gStyle.SetOptStat(0)
@@ -77,13 +97,17 @@ def printDiffPlots(scatter,diff,diffMap,suffix):
     #    a.GetXaxis()->SetTitle(suffix1);
     #    a.GetYaxis()->SetTitle(suffix2);
     c = ROOT.TCanvas ( "plots" )
+    scatter.GetXaxis().SetTitle(tag1)
+    scatter.GetYaxis().SetTitle(tag2)
     scatter.Draw("BOX")
     c.SaveAs("plotsFitDiff/corrAlpha_"+suffix+".png")
 
+    diff.GetXaxis().SetTitle(tag1+" - "+tag2)
     diff.Draw()
     diff.Fit("gaus","","",diff.GetMean()-3*diff.GetRMS(),diff.GetMean()+3*diff.GetRMS());
     c.SaveAs("plotsFitDiff/diffAlpha_"+suffix+".png")
 
+    diffMap.GetZaxis().SetTitle(tag1+" - "+tag2)
     diffMap.GetZaxis().SetRangeUser(1+diff.GetMean()-3*diff.GetRMS(),1+diff.GetMean()+3*diff.GetRMS());
     diffMap.Draw("COLZ");
     c.SaveAs("plotsFitDiff/diffAlphaMap_"+suffix+".png")
@@ -154,7 +178,10 @@ def combineChannels(channels,list1,list2,combined,strategy,status):
             if (err<0.3):
                 combined[channel]=(alpha,err,status,strategy)
             else:
-                combined[channel]=(1.52,-9.999,"bad_fit","forced_as_bad_fit_in_combination")
+                if det=="EB":
+                    combined[channel]=(1.52,-9.999,"bad_fit","forced_as_bad_fit_in_combination")
+                elif det=="EE":
+                    combined[channel]=(1.16,-9.999,"bad_fit","forced_as_bad_fit_in_combination")
         elif strategy == "ttPriorityFile1largeErr":
             alpha, err = list1[channel][0],list1[channel][1] #for large err trust only values from the TT
             if alpha>2.1:
@@ -163,8 +190,10 @@ def combineChannels(channels,list1,list2,combined,strategy,status):
                 alpha=0.7
             combined[channel]=(alpha,err,status,strategy)
         elif strategy == "badFitSimpleCombination":
-            combined[channel]=(1.52,-9.999,"bad_fit",strategy)
-                
+            if det=="EB":
+                combined[channel]=(1.52,-9.999,"bad_fit",strategy)
+            elif det=="EE":
+                combined[channel]=(1.16,-9.999,"bad_fit",strategy)
 def addChannels(channels,list,combined,status,message):
     for channel in channels:
         alpha, err = list[channel][0], list[channel][1]
@@ -175,9 +204,12 @@ def addChannels(channels,list,combined,status,message):
         if (err<0.3):
             combined[channel]=(alpha,err,status,message)
         else:
-            combined[channel]=(1.52,-9.999,"bad_fit","forced_as_bad_fit")
+            if det=="EB":
+                combined[channel]=(1.52,-9.999,"bad_fit","forced_as_bad_fit")
+            elif det=="EE":
+                combined[channel]=(1.16,-9.999,"bad_fit","forced_as_bad_fit")
 
-def badRegion2012Bv6():
+def badRegionEB2012Bv6():
     badChannels = set([ (int(x),int(y)) for x in range (26,46) for y in range(141,161) ])
     return badChannels
 
@@ -195,14 +227,17 @@ tag1 = argv.pop(1)
 tag2 = argv.pop(1)
 globalStrategy = argv.pop(1)
 
+det="EB"
+if (len(argv)>1):
+    det = argv.pop(1)
 globalSuffix = tag1+"_vs_"+tag2
 
 file1 = open(fn1)
 file2 = open(fn2)
 
-list1 = getlist( file1.read() )
+list1 = getlist( file1.read() , det)
 print "Read "+str(len(list1.keys()))+" values from file1 "+fn1  
-list2 = getlist( file2.read() )
+list2 = getlist( file2.read() , det)
 print "Read "+str(len(list2.keys()))+" values from file2 "+fn2  
 
 def cmp(x,y):
@@ -222,10 +257,14 @@ events2.sort( )
         
 commonScatterPlot = ROOT.TH2F("commonCorrelation","commonCorrelation",3000,0.,3.,3000,0.,3.)
 commonDiff = ROOT.TH1F("commonDiff","commonDiff",600,-3.,3.)
-commonMap = ROOT.TH2F("commonMap","commonMap",360,0.5,360.5,171,-85.5,85.5)
+if det=="EB":
+    commonMap = ROOT.TH2F("commonMap","commonMap",360,0.5,360.5,171,-85.5,85.5)
+elif det=="EE":
+    commonMap = ROOT.TH2F("commonMap","commonMap",200,0.5,200.5,100,0.5,100.5)
+    
 common = set(events1).intersection(  set(events2) )
-fillDiffPlots(common,list1,list2,commonScatterPlot,commonDiff,commonMap)
-printDiffPlots(commonScatterPlot,commonDiff,commonMap,"common")
+fillDiffPlots(common,list1,list2,commonScatterPlot,commonDiff,commonMap,det)
+printDiffPlots(commonScatterPlot,commonDiff,commonMap,"common"+globalSuffix)
 #
 only1 = list(set(events1) -  set(events2))
 only2 = list(set(events2) -  set(events1))
@@ -241,7 +280,7 @@ goodChannels2 = [ x for x in list2.keys() if list2[x][2] == "ok" ]
 goodChannels1.sort(  )
 goodChannels2.sort(  )
 
-#goodChannels1 = list(set(goodChannels1)-set(badRegion2012Bv6()))
+goodChannels1 = list(set(goodChannels1)-set(badRegionEB2012Bv6()))
 
 print "goodXtals in file1 " + str(len(goodChannels1))
 print "goodXtals in file2 " + str(len(goodChannels2))
@@ -263,8 +302,12 @@ combinedResult = {}
 
 goodScatterPlot = ROOT.TH2F("goodCorrelation","goodCorrelation",3000,0.,3.,3000,0.,3.)
 goodDiff = ROOT.TH1F("goodDiff","goodDiff",600,-3.,3.)
-goodMap = ROOT.TH2F("goodMap","goodMap",360,0.5,360.5,171,-85.5,85.5)
-fillDiffPlots(commonGood,list1,list2,goodScatterPlot,goodDiff,goodMap)
+if det=="EB":
+    goodMap = ROOT.TH2F("goodMap","goodMap",360,0.5,360.5,171,-85.5,85.5)
+elif det=="EE":
+    goodMap = ROOT.TH2F("goodMap","goodMap",200,0.5,200.5,100,0.5,100.5)
+    
+fillDiffPlots(commonGood,list1,list2,goodScatterPlot,goodDiff,goodMap,det)
 printDiffPlots(goodScatterPlot,goodDiff,goodMap,"goodCommon_"+globalSuffix)
 
 largeErrChannels1 = [ x for x in list1.keys() if list1[x][2] == "large_err_fit" ]
@@ -276,7 +319,7 @@ largeErrChannels2.sort(  )
 largeErrChannels1 = list(set(largeErrChannels1)-channelsDone)
 largeErrChannels2 = list(set(largeErrChannels2)-channelsDone)
 
-#largeErrChannels1 = list(set(largeErrChannels1)-set(badRegion2012Bv6()))
+largeErrChannels1 = list(set(largeErrChannels1)-set(badRegionEB2012Bv6()))
 
 
 
@@ -299,8 +342,11 @@ print "largeErrXtals only in file2 " + str(len(onlyLargeErr2))
 
 largeErrScatterPlot = ROOT.TH2F("largeErrCorrelation","largeErrCorrelation",3000,0.,3.,3000,0.,3.)
 largeErrDiff = ROOT.TH1F("largeErrDiff","largeErrDiff",600,-3.,3.)
-largeErrMap = ROOT.TH2F("largeErrMap","largeErrMap",360,0.5,360.5,171,-85.5,85.5)
-fillDiffPlots(commonLargeErr,list1,list2,largeErrScatterPlot,largeErrDiff,largeErrMap)
+if det=="EB":
+    largeErrMap = ROOT.TH2F("largeErrMap","largeErrMap",360,0.5,360.5,171,-85.5,85.5)
+elif det=="EE":
+    largeErrMap = ROOT.TH2F("largeErrMap","largeErrMap",200,0.5,200.5,100,0.5,100.5)
+fillDiffPlots(commonLargeErr,list1,list2,largeErrScatterPlot,largeErrDiff,largeErrMap,det)
 printDiffPlots(largeErrScatterPlot,largeErrDiff,largeErrMap,"largeErrCommon_"+globalSuffix)
 
 badFitChannels1 = [ x for x in list1.keys() if list1[x][2] == "bad_fit" ]
@@ -328,8 +374,11 @@ print "badFitXtals only in file2 " + str(len(onlyBadFit2))
 
 badFitScatterPlot = ROOT.TH2F("badFitCorrelation","badFitCorrelation",3000,0.,3.,3000,0.,3.)
 badFitDiff = ROOT.TH1F("badFitDiff","badFitDiff",600,-3.,3.)
-badFitMap = ROOT.TH2F("badFitMap","badFitMap",360,0.5,360.5,171,-85.5,85.5)
-fillDiffPlots(commonBadFit,list1,list2,badFitScatterPlot,badFitDiff,badFitMap)
+if det=="EB":
+    badFitMap = ROOT.TH2F("badFitMap","badFitMap",360,0.5,360.5,171,-85.5,85.5)
+if det=="EE":
+    badFitMap = ROOT.TH2F("badFitMap","badFitMap",200,0.5,200.5,100,0.5,100.5)
+fillDiffPlots(commonBadFit,list1,list2,badFitScatterPlot,badFitDiff,badFitMap,det)
 printDiffPlots(badFitScatterPlot,badFitDiff,badFitMap,"badFitCommon_"+globalSuffix)
 
 
@@ -347,15 +396,24 @@ print "Final channels in combination:"+str(len(combinedResult.keys()))
 
 combinedValues = ROOT.TH1F("combinedValues","combinedValues",1000,0.,3.)
 combinedErr = ROOT.TH1F("combinedErr","combinedErr",1000,0.,0.3)
-combinedMap = ROOT.TH2F("combinedMap","combinedMap",360,0.5,360.5,171,-85.5,85.5)
-fillAlphaPlots(combinedResult,combinedValues,combinedErr,combinedMap)
+if det=="EB":
+    combinedMap = ROOT.TH2F("combinedMap","combinedMap",360,0.5,360.5,171,-85.5,85.5)
+elif det=="EE":
+    combinedMap = ROOT.TH2F("combinedMap","combinedMap",200,0.5,200.5,100,0.5,100.5)
+fillAlphaPlots(combinedResult,combinedValues,combinedErr,combinedMap,det)
 printAlphaPlots(combinedValues,combinedErr,combinedMap,globalSuffix)
 
 out=open("fitCombined_"+globalSuffix+".txt","w")
 print>>out, "## File generated from combination of "+fn1+" and "+fn2+" using strategy "+globalStrategy
-out.write("##\tieta\tiphi\talpha\talpha_err\tstatus\tcomment\n")
+if det=="EB":
+    out.write("##\tieta\tiphi\talpha\talpha_err\tstatus\tcomment\n")
+elif det=="EE":
+    out.write("##\tix\tiy\tsign\talpha\talpha_err\tstatus\tcomment\n")
 channels=combinedResult.keys()
 channels.sort()
 for key in channels:
-    out.write("\t%d\t%d\t%f\t%f\t%s\t%s\n"% (key[0],key[1],combinedResult[key][0],combinedResult[key][1],combinedResult[key][2],combinedResult[key][3]) )
+    if det=="EB":
+        out.write("\t%d\t%d\t%f\t%f\t%s\t%s\n"% (key[0],key[1],combinedResult[key][0],combinedResult[key][1],combinedResult[key][2],combinedResult[key][3]) )
+    elif det=="EE":
+        out.write("\t%d\t%d\t%d\t%f\t%f\t%s\t%s\n"% (key[0],key[1],key[2],combinedResult[key][0],combinedResult[key][1],combinedResult[key][2],combinedResult[key][3]) )
 print "Written combination file fitCombined_"+globalSuffix+".txt"
