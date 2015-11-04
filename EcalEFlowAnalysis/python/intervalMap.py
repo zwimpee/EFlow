c838f1b9901c9040c108adb2864f23f314c6f332
Paolo Meridiani <paolo.meridiani@cern.ch>
2015-11-04 17:26:09 +0100

    fixed intervalMap

--- EcalEFlowAnalysis/python/intervalMap.py
+++ EcalEFlowAnalysis/python/intervalMap.py
@@ -5,15 +5,15 @@ from ROOT import TTree, TFile
 
 class intervalMap:    
     def __init__(self, mapFile):
-        self.mapFile=ROOT.TFile.Open(mapFile,'read')
+        self.mapFile=TFile.Open(mapFile,'read')
         intervals={}
         for interval in self.mapFile.outTree_barl:
             intervals[interval.index]={"firstRun":interval.firstRun,"firstLumi":interval.firstLumi,"lastRun":interval.lastRun,"lastLumi":interval.lastLumi}
-        self.intervalMap=collections.OrderedDict(sorted(interval.items()))
-        print intervalMap
+        self.intervals=collections.OrderedDict(sorted(intervals.items()))
+        print "Initialized intervalMap from "+str(mapFile)
 
     def intervalIndex(self, run, lumi):
-        for key,value in self.intervalMap.iteritems():
+        for key,value in self.intervals.iteritems():
             if run>=value["firstRun"] and run<=value["lastRun"]:
                 if (value["firstRun"] != value["lastRun"]):
                     return key
